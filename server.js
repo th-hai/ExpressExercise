70ec708080efe703d280d39c3b95cb62b85fd46b
Glitch (using-lowdb) <none>
2020-05-13 03:09:35 +0000

    🏆💒 Lesson 6: Using lowdb

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
+var express = require("express");
 var bodyParser = require("body-parser");
-const app = express();
+var app = new express();
+var low = require("lowdb");
+var FileSync = require("lowdb/adapters/FileSync");
+var adapter = new FileSync("db.json");
+
+var db = low(adapter);
+
+db.defaults({ todos: [] }).write();
+
+var port = 3000;
 
 app.set("view engine", "pug");
 app.set("views", "./views");
@@ -16,33 +20,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-var todoLists = [
-  { todo: "Đi chợ" },
-  { todo: "Nấu cơm" },
-  { todo: "Rửa bát" },
-  { todo: "Học code tại CodersX" }
-];
-
-// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(req, res) {
   res.render("index", {
-    todoLists: todoLists
+    todos: db.get('todos').value()
   });
 });
 
 app.get("/todos", function(req, res) {
-  res.render("todo", {
-    todoLists: todoLists
+  res.render("index", {
+    todos: db.get('todos').value()
   });
 });
 
-app.get("/todos", function(req, res) {
+app.get("/todos/search", function(req, res) {
   var q = req.query.q;
-  var matchedTask = todoLists.filter(function(task) {
-    return task.todo.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  // Get data from db
+  var todoList = db.get('todos').value();
+  // Filter by query
+  var matchedToDo = todoList.filter(function(task) {
+    return task.text.toLowerCase().indexOf(q.toLowerCase()) !== -1;
   });
-  res.render("todos", {
-    todoLists: matchedTask
+  res.render("index", {
+    todos: matchedToDo
   });
 });
 
@@ -51,11 +50,9 @@ app.get("/todos/create", function(req, res) {
 });
 
 app.post("/todos/create", function(req, res) {
-  todoLists.push(req.body);
+  db.get('todos').push(req.body).write();
   res.redirect("/todos");
 });
-
-// listen for requests :)
-app.listen(process.env.PORT, () => {
-  console.log("Server listening on port " + process.env.PORT);
+app.listen(port, function(req, res) {
+  console.log("Server listening on port " + port);
 });
